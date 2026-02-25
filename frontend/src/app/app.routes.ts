c39d0013ea124873a13b3ae668e9030cbe6928c1
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-25 09:20:22 +0100

    add componente mis-tareas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { C404 } from './components/c404/c404';
 import { Registro } from './components/registro/registro';
 import { Login } from './components/login/login';
 import { Confirmar } from './components/confirmar/confirmar';
+import { MisTareas } from './components/mis-tareas/mis-tareas';
 
 export const routes: Routes = [
     {path:'',pathMatch:'full',redirectTo:'home'},
     {path:'home',component:Home},
+    {path:'mis-tareas',component:MisTareas},
     {path:'registro',component:Registro},
     {path:'login',component:Login},
     {path:'confirmar/:token',component:Confirmar},
---
+++ frontend/src/app/components/mis-tareas/mis-tareas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-tareas',
+  imports: [],
+  templateUrl: './mis-tareas.html',
+  styleUrl: './mis-tareas.css',
+})
+export class MisTareas {
+
+}
